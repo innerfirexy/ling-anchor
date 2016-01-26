dff797110f0c26000dade3f51685150fccd5a56e
Yang Xu <innerfirexy@gmail.com>
2016-01-25 21:15:12 -0500

    write some to wiki_compute_dist.r

--- wiki_compute_dist.r
+++ wiki_compute_dist.r
@@ -2,3 +2,11 @@
 # Yang Xu
 # 1/25/2016
 
+library(data.table)
+library(dplyr)
+
+df.pairs = readRDS('wiki.pairs.rds')
+dt.pairs = data.table(df.pairs, key = c('primeUtterID', 'targetUtterID'))
+
+dt.ub = readRDS('wiki.user.baseline.rds')
+
